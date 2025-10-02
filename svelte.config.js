7e2212180b8685d3192915bec35c0ffda431809c
gourav <gourav@auvo.io>
2025-10-02 23:44:22 +0530

    fix: deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
+const dev = process.env.NODE_ENV === 'development';
+
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      // fallback allows SPA behavior for dynamic routes
+      fallback: 'index.html'
+    }),
+    paths: {
+      base: dev ? '' : '/shiva_builders'
+    }
+  }
 };
 
 export default config;
