0c2b3ce8314b14adfe2615d867419f5575f71ba9
gourav <gourav@auvo.io>
2025-10-03 00:36:32 +0530

    Updates

--- _app/immutable/chunks/BdxVaOco.js
+++
@@ -1,1 +0,0 @@
-import{e as d,i as n,h as u,Y as c,Z as i}from"./DiSSCXU0.js";function y(s,o,r){d(()=>{var a=n(()=>o(s,r?.())||{});if(r&&a?.update){var t=!1,f={};u(()=>{var e=r();c(e),t&&i(f,e)&&(f=e,a.update(e))}),t=!0}if(a?.destroy)return()=>a.destroy()})}export{y as a};
--- _app/immutable/chunks/CFaWOaLI.js
+++
@@ -1,9 +0,0 @@
-import"./DsnmJJEf.js";import{c as F,f as G,a as i,b as y,d as b,n as f,r as m,t as u,g as h,u as H}from"./DiSSCXU0.js";import{d as I}from"./Dl4eaSY_.js";import{s as v}from"./DyTQcDbA.js";import{p as s,i as J}from"./BDDKT6Ck.js";import{s as l,a as p,c as _}from"./CbRkzXNG.js";var K=y('<a role="button"><!></a>'),L=y("<button><!></button>");function S(g,t){let w=s(t,"variant",3,"primary"),n=s(t,"href",3,null),x=s(t,"type",3,"button"),r=s(t,"disabled",3,!1),C=s(t,"customClass",3,""),k=s(t,"handleClick",3,()=>{});const B="font-inter text-base font-semibold px-6 py-3 rounded transition-all duration-200 ease-in-out cursor-pointer text-center whitespace-nowrap",j=`
-    bg-yellow-500 text-black-900 
-    hover:bg-yellow-300
-    active:scale-[0.98]
-  `,q=`
-    bg-transparent text-white border-2 border-white
-    hover:bg-white hover:text-black-900 
-    active:scale-[0.98]
-  `,c=H(()=>`${B} ${w()==="primary"?j:q} ${r()?"opacity-50 cursor-not-allowed":""} ${C()}`);var d=F(),z=G(d);{var A=e=>{var a=K(),o=b(a);v(o,()=>t.children??f),m(a),u(()=>{l(a,"href",n()),p(a,1,_(h(c))),l(a,"aria-disabled",r())}),i(e,a)},D=e=>{var a=L();a.__click=function(...E){k()?.apply(this,E)};var o=b(a);v(o,()=>t.children??f),m(a),u(()=>{l(a,"type",x()),a.disabled=r(),p(a,1,_(h(c)))}),i(e,a)};J(z,e=>{n()?e(A):e(D,!1)})}i(g,d)}I(["click"]);export{S as B};
