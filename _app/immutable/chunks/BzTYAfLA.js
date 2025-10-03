66774dc5bbf17cfa26b2e924c43e8cc4b395e42d
gourav <gourav@auvo.io>
2025-10-03 13:54:28 +0530

    Updates

--- _app/immutable/chunks/BzTYAfLA.js
+++ _app/immutable/chunks/BzTYAfLA.js
@@ -1,4 +1,5 @@
-import"./DsnmJJEf.js";import{b as m,t as f,a as g,d as t,s as d,r as a}from"./BAxAQHQ7.js";import{s as b}from"./fp8YgKhm.js";import{s as i}from"./C6ddKZd8.js";import{H as w,s as p}from"./BGSjYmJI.js";var x=m(`<a class="
+import"./DsnmJJEf.js";import{b as f,t as m,a as g,d as t,s as d,r as a}from"./BAxAQHQ7.js";import{s as b}from"./fp8YgKhm.js";import{s as i}from"./C6ddKZd8.js";import{H as w,s as p}from"./BGSjYmJI.js";var x=f(`<a class="
+	h-full
     group relative
     flex w-80
     max-w-sm
@@ -19,4 +20,4 @@ import"./DsnmJJEf.js";import{b as m,t as f,a as g,d as t,s as d,r as a}from"./BA
       group-hover:scale-110
       group-hover:bg-yellow-500
       group-hover:text-black-900
-    "><!></div></div> <div class="p-6"><h4 class="font-semibold text-white transition-colors duration-300 group-hover:text-yellow-500"> </h4></div></a>`);function C(c,e){var r=x(),o=t(r),s=t(o),l=d(s,4),u=t(l);w(u,{get icon(){return p},size:24,color:"currentColor",strokeWidth:1.5}),a(l),a(o);var n=d(o,2),v=t(n),h=t(v,!0);a(v),a(n),a(r),f(()=>{i(r,"href",e.href),i(s,"src",e.imageUrl),i(s,"alt",e.title),b(h,e.title)}),g(c,r)}export{C as P};
+    "><!></div></div> <div class="p-6"><h4 class="font-semibold text-white transition-colors duration-300 group-hover:text-yellow-500"> </h4></div></a>`);function C(c,e){var r=x(),o=t(r),s=t(o),l=d(s,4),u=t(l);w(u,{get icon(){return p},size:24,color:"currentColor",strokeWidth:1.5}),a(l),a(o);var n=d(o,2),v=t(n),h=t(v,!0);a(v),a(n),a(r),m(()=>{i(r,"href",e.href),i(s,"src",e.imageUrl),i(s,"alt",e.title),b(h,e.title)}),g(c,r)}export{C as P};
--- _app/immutable/entry/start.De0hgFuK.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/z8Yq7lNJ.js";export{o as load_css,r as start};
